3d48353ceb8beee2d845277645f983ba7d6121c6
jhpoelen <jhpoelen@xs4all.nl>
2017-06-27 23:53:44 -0700

    rename test

---
+++ test/linecheck.js
@@ -0,0 +1,32 @@
+var test = require('tape');
+var mqtt = require('mqtt')
+
+var assertPubSub = function(t, url) {
+  var client  = mqtt.connect(url)
+
+  client.on('connect', function () {
+    client.subscribe('presence');
+    client.publish('presence', 'Hello mqtt');
+  })
+
+  client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log(message.toString());
+    client.end();
+    t.equal(message.toString(), 'Hello mqtt');
+  })
+
+}
+
+test('heroku linecheck', function(t) {
+  t.plan(1);
+  assertPubSub(t, 'ws://meshygardenshed.herokuapp.com');
+});
+
+
+test('pub linecheck', function(t) {
+  t.plan(1);
+  assertPubSub(t, 'mqtt://test.mosca.io');
+});
+
+
