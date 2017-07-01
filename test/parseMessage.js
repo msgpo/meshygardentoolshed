e52bd8434a327308c72174829ad36c61e07f6eac
jhpoelen <jhpoelen@xs4all.nl>
2017-06-30 20:46:44 -0700

    parse another message

--- test/parseMessage.js
+++ test/parseMessage.js
@@ -1,13 +1,30 @@
 var test = require('tape');
 
-test('parse message', function(t) {
-  t.plan(5);
-  var msg = Buffer.from([0x74, 0x65, 0x6d, 0x70, 0x09, 0x31, 0x09, 0x32, 0x35, 0x2e, 0x38, 0x30, 0x30, 0x09, 0x43, 0x0a]);
-    t.equals(msg.toString(), 'temp\t1\t25.800\tC\n');
-    var parts = msg.toString().trim().split('\t');
-    var m = { type: parts[0], seq: parts[1], value: 1*parts[2], unit: parts[3]};
+var parse = function (buffer) {
+    var parts = buffer.toString().trim().split('\t');
+    var msg = undefined;
+    if (parts.length > 3) {
+        msg = { type: parts[0], seq: parts[1], value: 1 * parts[2], unit: parts[3]};
+    }
+    return msg;
+};
+
+test('parse message', function (t) {
+    t.plan(4);
+    var msg = Buffer.from([0x74, 0x65, 0x6d, 0x70, 0x09, 0x31, 0x09, 0x32, 0x35, 0x2e, 0x38, 0x30, 0x30, 0x09, 0x43, 0x0a]);
+    var m = parse(msg);
     t.equals(m.type, 'temp');
     t.equals(m.seq, '1');
     t.equals(m.value, 25.8);
+    t.equals(m.unit, 'C');
+});
+
+test('parse another message', function (t) {
+    t.plan(4);
+    var msg = Buffer.from([0x74, 0x65, 0x6d, 0x70, 0x09, 0x38, 0x09, 0x33, 0x30, 0x2e, 0x37, 0x30, 0x30, 0x09, 0x43, 0x0a]);
+    var m = parse(msg);
+    t.equals(m.type, 'temp');
+    t.equals(m.seq, '8');
+    t.equals(m.value, 30.7);
     t.equals(m.unit, 'C');
 });
