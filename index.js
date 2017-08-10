0a7ea04b29503ec11299a3138bad4ed51f1e67e0
Sierk Beij <sierk@cococollective.org>
2017-08-09 17:12:21 -0700

    Reformatted code.

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 var mosca = require('mosca');
 var message = require('./message');
 
-var moscaSettings = function() {
-  return { port: 1883, host: '0.0.0.0' };
-}
+var moscaSettings = function () {
+    return {port: 1883, host: '0.0.0.0'};
+};
 
 var server = new mosca.Server(moscaSettings());
 server.on('ready', setup);
 
-server.on('clientConnected', function(client) {
-  logError('client connected', client.id);
+server.on('clientConnected', function (client) {
+    logError('client connected', client.id);
 });
 
 // fired when a message is received
-server.on('published', function(packet) {
-  console.error('Published', packet.payload);
-  var data = message.parse(packet.payload);
-  if (data.length < 1) {
-      logError('failed to parse [' + packet.payload + ']');
-  }
-  else {
-    data.map(message.toTsv).forEach(function (msg) {
-        console.log(msg);
-    })
-  }
+server.on('published', function (packet) {
+    console.error('Published', packet.payload);
+    var data = message.parse(packet.payload);
+    if (data.length < 1) {
+        logError('failed to parse [' + packet.payload + ']');
+    }
+    else {
+        data.map(message.toTsv).forEach(function (msg) {
+            console.log(msg);
+        })
+    }
 });
 
-server.on('error', function(client) {
-  logError('some errors');
+server.on('error', function (client) {
+    logError('some errors');
 });
 
 // fired when the mqtt server is ready
 function setup() {
-  logError('Mosca server is up and running')
+    logError('Mosca server is up and running')
 }
 
-var logError = function(msg) {
-  console.error('[' + new Date() + ']: [' + msg + ']');
-}
+var logError = function (msg) {
+    console.error('[' + new Date() + ']: [' + msg + ']');
+};
 
 
 //module.exports = { parse: parse };
--- test/linecheck.js
+++ test/linecheck.js
@@ -1,31 +1,31 @@
 var test = require('tape');
 var mqtt = require('mqtt');
 
-var assertPubSub = function(t, url, msg) {
-  var text = msg || 'Hello mqtt';
-  var client  = mqtt.connect(url);
+var assertPubSub = function (t, url, msg) {
+    var text = msg || 'Hello mqtt';
+    var client = mqtt.connect(url);
 
-  client.on('connect', function () {
-    client.subscribe('presence');
-    client.publish('presence', text);
-  });
+    client.on('connect', function () {
+        client.subscribe('presence');
+        client.publish('presence', text);
+    });
 
-  client.on('message', function (topic, message) {
-    // message is Buffer
-    console.log(message.toString());
-    client.end();
-    t.equal(message.toString(), text);
-  });
+    client.on('message', function (topic, message) {
+        // message is Buffer
+        console.log(message.toString());
+        client.end();
+        t.equal(message.toString(), text);
+    });
 };
 
-test('pub linecheck', function(t) {
-  t.plan(1);
-  assertPubSub(t, 'mqtt://test.mosca.io');
+test('pub linecheck', function (t) {
+    t.plan(1);
+    assertPubSub(t, 'mqtt://test.mosca.io');
 });
 
-test('peoples linecheck', function(t) {
-  t.plan(1);
-  assertPubSub(t, 'mqtt://peoplesopen.net', '{"data":{"humi":{"value":"60.400","type":"humi","unit":"pct"},"temp":{"value":"22.0","type":"temp","unit":"C"},"soil":{"value":757,"type":"soil","unit":"pct"}},"source":11605683}');
+test('peoples linecheck', function (t) {
+    t.plan(1);
+    assertPubSub(t, 'mqtt://peoplesopen.net', '{"data":{"humi":{"value":"60.400","type":"humi","unit":"pct"},"temp":{"value":"22.0","type":"temp","unit":"C"},"soil":{"value":757,"type":"soil","unit":"pct"}},"source":11605683}');
 });
 
 
