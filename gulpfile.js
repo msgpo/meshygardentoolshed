001cb04c20bd5612511c9968a08db454531da993
Sierk Beij <sierk@cococollective.org>
2017-08-10 00:58:21 -0700

    Added browserify, babel, and gulp for ES6-ing the web js.

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp        = require('gulp');
+var browserify  = require('browserify');
+var babelify    = require('babelify');
+var source      = require('vinyl-source-stream');
+var buffer      = require('vinyl-buffer');
+var uglify      = require('gulp-uglify');
+
+gulp.task('build', function () {
+    return browserify({entries: './assets/src/js/visualise.js', debug: true})
+        .transform("babelify", { presets: ["es2015"] })
+        .bundle()
+        .pipe(source('visualise.min.js'))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(gulp.dest('./web/js'));
+});
+
+gulp.task('watch', ['build'], function () {
+    gulp.watch('./assets/src/js/*.js', ['build']);
+});
+
+gulp.task('default', ['build', 'watch']);
