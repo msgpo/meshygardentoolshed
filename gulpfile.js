eb270bfd01b063ba8fdeed88330a7b7e03116984
Sierk Beij <sierk@cococollective.org>
2017-08-10 01:05:08 -0700

    Temporarily disabling uglifying of visualise.js.

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,16 @@ var gulp        = require('gulp');
 var browserify  = require('browserify');
 var babelify    = require('babelify');
 var source      = require('vinyl-source-stream');
-var buffer      = require('vinyl-buffer');
-var uglify      = require('gulp-uglify');
+// var buffer      = require('vinyl-buffer');
+// var uglify      = require('gulp-uglify');
 
 gulp.task('build', function () {
     return browserify({entries: './assets/src/js/visualise.js', debug: true})
         .transform("babelify", { presets: ["es2015"] })
         .bundle()
         .pipe(source('visualise.min.js'))
-        .pipe(buffer())
-        .pipe(uglify())
+        // .pipe(buffer())
+        // .pipe(uglify())
         .pipe(gulp.dest('./web/js'));
 });
 
