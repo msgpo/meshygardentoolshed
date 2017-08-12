9a57550b1ddc1ede51482d970e2bac81f2f28b14
Sierk Beij <sierk@cococollective.org>
2017-08-12 12:54:37 -0700

    Uglified javascript for chart.

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,16 @@ const gulp        = require('gulp');
 const browserify  = require('browserify');
 const babelify    = require('babelify');
 const source      = require('vinyl-source-stream');
-// const buffer      = require('vinyl-buffer');
-// const uglify      = require('gulp-uglify');
+const buffer      = require('vinyl-buffer');
+const uglify      = require('gulp-uglify');
 
 gulp.task('build', function () {
     return browserify({entries: './assets/src/js/visualise.js', debug: true})
         .transform("babelify", { presets: ["es2015"] })
         .bundle()
         .pipe(source('visualise.min.js'))
-        // .pipe(buffer())
-        // .pipe(uglify())
+        .pipe(buffer())
+        .pipe(uglify())
         .pipe(gulp.dest('./web/js'));
 });
 
